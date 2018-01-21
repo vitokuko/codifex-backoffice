d36262864130b49031d8c2c5571aadce40890f36
souaibou <ndiayevito04@gmail.com>
2018-01-21 16:47:39 +0000

    origin
    part of traitement dataService

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -1,11 +1,12 @@
-///<reference path="../../node_modules/rxjs/add/operator/map.d.ts"/>
 import { Injectable } from '@angular/core';
 import {Http, Headers, Response} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class DataServiceService {
 
-  baseUrl = '';
+  baseUrl = 'https://codifex-api.herokuapp.com/api/';
 
   private headers = new Headers({'Content-Type': 'application/json'});
 
@@ -15,22 +16,32 @@ export class DataServiceService {
 
   getData(url){
     return this.http.get(this.baseUrl + url)
-      .map((res:Response) => res.json());
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'error server'));
   }
 
   addData(url,data){
+    console.log(JSON.stringify(data));
     return this.http.post(this.baseUrl + url, JSON.stringify(data), {headers: this.headers})
-      .map((res:Response) => res.json());
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'error server'));
   }
 
   patchData(url,data){
     return this.http.patch(this.baseUrl + url + "/" + data.id, data)
-      .map((res:Response) => res.json());
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'error server'));
   }
 
   deleteData(url,dataId){
     return this.http.delete(this.baseUrl + url + "/" + dataId)
-      .map((res:Response) => res.json());
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'error server'));
+  }
+  putData(url,data){
+    return this.http.put(this.baseUrl + url, JSON.stringify(data), {headers: this.headers})
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'error server'));
   }
 
 }
